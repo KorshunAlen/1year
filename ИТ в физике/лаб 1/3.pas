ec1c80816c6818b1a3e531d7f350ba92445adec5
KorshunAlen <141930201+KorshunAlen@users.noreply.github.com>
2026-06-01 23:29:13 +0300

    Add files via upload

---
+++ ИТ в физике/лаб 1/3.pas
@@ -0,0 +1,16 @@
+﻿program al;
+var     Q :integer;
+        a, l, d, c, F: real;
+   
+begin
+    a:= 13.5;
+    l:= 3;
+    d:= pi*a/l;
+    c:=(pi/2)*(pi/2);
+
+    for Q:= 0 to 90 do
+      begin
+        F:=((1+sin(Q))*cos(d*cos(Q)))/(c-(d*cos(Q))*(d*cos(Q)));
+        writeln('Q=',Q,' ','F=',F);
+      end;
+end.
---
+++ ИТ в физике/лаб 1/прога.pas
@@ -0,0 +1,24 @@
+﻿program graf;
+uses GraphABC;
+var
+  x,y: integer;
+  
+  r1:= 1.52;
+  r2:= 1;
+  P1:=687;
+  w1:= 2*Pi/P1;
+  P2:=365;
+  w2:=2*Pi/P2;
+  f:=0;
+  
+begin
+  setwindowsize(1000, 1000);
+  line(160,0,160,300);
+  line(0,160,320,160);
+  for t:integer:=1 to 12000 do
+  begin
+    x:= integer((r1*cos(w1*t +f) - r2*cos(w2*t+f))*50);
+    y:= integer((r1*sin(w1*t +f) - r2*sin(w2*t+f))*50);
+    SetPixel(x+160,160-y,RGB (0,0,0));
+  end;
+end.
